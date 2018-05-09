3a3c7bfc0009efafaeb5384e9abc05613554a888
Jinggy <31829164+jb2548@users.noreply.github.com>
2018-05-08 21:21:01 -0400

    Delete index.js

--- App/src/components/screens/index.js
+++
@@ -1,7 +0,0 @@
-import MainFeed from "./MainFeed";
-import Login from "./Login";
-import Profile from "./Profile";
-import Saved from "./Saved"; 
-import Register from "./Register"
-
-export { MainFeed, Login, Profile, Saved, Register }; 
